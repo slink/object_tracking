85d7fabc36650584b8a8f52d8813f55330c85e26
!link <sglink@gmail.com>
2014-02-17 14:40:10 -0800

    clean-up and add video_to_stills

--- ffmpeg_functions.py
+++ ffmpeg_functions.py
@@ -1,10 +1,65 @@
+import glob, re, os, shutil
+from scipy.misc import imread, imsave
+import numpy as np
+import subprocess as sp
+from datetime import datetime, timedelta
+
+
+def video_to_stills(FFMPEG_BINARY, filename, start = NaN, finish = NaN, folder):
+    
+    if math.isnan(start):
+        print 'video start set at 0 sec.'
+        start = HHMMSS(0)
+    if math.isnan(finish):
+        print 'assuming length of test for duration'
+        finish = HHMMSS(video_duration(FFMPEG_BINARY, filename))
+
+    fps = video_fps(FFMPEG_BINARY, filename)
+
+    calling_dir = os.getcwd()
+    folder_path = os.path.join(calling_dir, folder)
+    
+    if not os.path.exists(folder_path):
+        os.mkdir(folder)
+
+    try:
+        os.chdir(folder_path)
+    except:
+        raise
+
+    try:
+        shutil.copy2(filename, folder_path)
+    except:
+        raise
+
+    proc = sp.Popen([FFMPEG_BINARY, '-ss', start,
+                    '-t', finish, '-i', filename, 
+                    '-r', fps, 'test%4d.jpg'],
+                    stdin=sp.PIPE,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+
+    proc.stdout.readline()
+    proc.terminate()
+
+    try:
+        os.remove(filename)
+    except:
+        raise
+
+    try:
+        os.chdir(calling_dir)
+    except:
+        raise
+
+
 def load_video_data(FFMPEG_BINARY, filename):
     # This function is substantially based on one found at http://zulko.github.io/moviepy/
     # open the file in a pipe, provoke an error, read output
     proc = sp.Popen([FFMPEG_BINARY, "-i", filename, "-"],
-        stdin=sp.PIPE,
-        stdout=sp.PIPE,
-        stderr=sp.PIPE)
+                    stdin=sp.PIPE,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
     proc.stdout.readline()
     proc.terminate()
     infos = proc.stderr.read()
@@ -38,7 +93,7 @@ def video_fps(FFMPEG_BINARY, filename):
     fps = float(line[match.start():match.end()].split(' ')[1])
     return fps
 
-def video_duration(FFMPEG_BINARY, filename)):
+def video_duration(FFMPEG_BINARY, filename):
     # This function is substantially based on one found at http://zulko.github.io/moviepy/
     line = load_video_data(FFMPEG_BINARY, filename)
 
@@ -47,8 +102,8 @@ def video_duration(FFMPEG_BINARY, filename)):
     match = re.search(" [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9]", line)
     hms = map(float, line[match.start()+1:match.end()].split(':'))
     duration = cvsecs(*hms)
-
-# nframes = int(duration*fps)
+    return duration
+    # nframes = int(duration*fps)
     
 def cvsecs(*args):
     # This function is substantially based on one found at http://zulko.github.io/moviepy/
@@ -66,3 +121,10 @@ def cvsecs(*args):
         return 60*args[0]+args[1]
     elif len(args) ==3:
         return 3600*args[0]+60*args[1]+args[2]
+
+def HHMMSS(sec):
+    # based on http://stackoverflow.com/a/4048773/1902480
+    sec = timedelta(sec)
+    d = datetime(1900,1,1) + sec
+    
+    return '{:%H:%M:%S}'.format(d)
